ae4152cef82174039953ecdfae36dee1e96afe8c
YukkiMoru <136158980+YukkiMoru@users.noreply.github.com>
2025-06-13 20:37:57 +0900

    WIP add : SmartLEDクラスのコンストラクタを簡素化し、関連コードを整理

--- invistigate/BNO085_NEOPIXEL/lib/SmartLED/SmartLED.h
+++ invistigate/BNO085_NEOPIXEL/lib/SmartLED/SmartLED.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <Adafruit_NeoPixel.h>
 
+// 必要に応じて値を変更
+#define SLED_NUMPIXELS  1
+#define SLED_PIN        6
+#define SLED_BRIGHTNESS 20
+
 enum SmartLEDState {
     SLED_Init,
     SLED_Error,
@@ -9,7 +14,11 @@ enum SmartLEDState {
 
 class SmartLED {
 public:
-    SmartLED(Adafruit_NeoPixel& pixels) : _pixels(pixels), _state(SLED_Init) { update(); }
+    SmartLED() : _pixels(SLED_NUMPIXELS, SLED_PIN, NEO_GRB + NEO_KHZ800), _state(SLED_Init) {
+        _pixels.begin();
+        _pixels.setBrightness(SLED_BRIGHTNESS); // ← ここで明るさも設定
+        update();
+    }
     void setState(SmartLEDState state) {
         if (_state != state) {
             _state = state;
@@ -17,13 +26,13 @@ public:
         }
     }
 private:
-    Adafruit_NeoPixel& _pixels;
+    Adafruit_NeoPixel _pixels;
     SmartLEDState _state;
     void update() {
         uint32_t color = 0;
         switch (_state) {
-            case SLED_Init: color = _pixels.Color(255, 80, 0); break;
-            case SLED_Error:        color = _pixels.Color(255, 0, 0); break;
+            case SLED_Init:  color = _pixels.Color(255, 80, 0); break;
+            case SLED_Error: color = _pixels.Color(255, 0, 0); break;
             case SLED_Run:   color = _pixels.Color(0, 255, 0); break;
         }
         _pixels.fill(color);
--- invistigate/BNO085_NEOPIXEL/src/main.cpp
+++ invistigate/BNO085_NEOPIXEL/src/main.cpp
@@ -1,25 +1,12 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 #include <SmartLED.h>
 
-#define NUMPIXELS 1
-
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-SmartLED statusLED(pixels);
+SmartLED statusLED;
 
 void setup() {
   Serial.begin(115200);
 
-#if defined(NEOPIXEL_POWER)
-  pinMode(NEOPIXEL_POWER, OUTPUT);
-  digitalWrite(NEOPIXEL_POWER, HIGH);
-#endif
-
-  pixels.begin();
-  pixels.setBrightness(20);
-  delay(10); // 初期化の安定化
   statusLED.setState(SLED_Init);
-  pixels.show(); // 明示的にshowを呼ぶ
   delay(1000);
 }
 
